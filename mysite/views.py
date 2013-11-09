506252681f03db9bcad3a7b43082c9caa0b37408
Xbluer <xusi1122@gmail.com>
2013-11-10 00:50:52 +0800

    remove something in ch03

--- mysite/views.py
+++ mysite/views.py
@@ -4,17 +4,4 @@ import datetime
 def hello(request):
     return HttpResponse("Hello world")
 
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><head><title>time</title></head><body>It is new %s.</body></html>" % now
-    return HttpResponse(html)
-def hours_ahead(request, offset):
-    try:
-        offset = int(offset)
-    except ValueError:
-        raise Http404()
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    #assert False
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
 
