497904090edf3b323e75c08bf959fa5dc9385dcc
Xbluer <xusi1122@gmail.com>
2013-11-11 20:26:02 +0800

    "search result DONE not good"

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
+from books.models import Book
 
 def search_form(request):
     return render_to_response('search_form.html')
@@ -8,9 +9,14 @@ def search_form(request):
 def search(request):
     if 'q' in request.GET:
         if request.GET['q']:
-            message = 'You searched for: %s' % request.GET['q']
+            q = request.GET['q']
+            books = Book.objects.filter(title__icontains = q)
+            return render_to_response('search_result.html', 
+                                      {'books' : books, 'query': q})
         else:
-            message = 'You submitted an empty form.'
+            #message = 'You submitted an empty form.'
+            return render_to_response('search_form.html',
+                                      {'error': True})
     else:
         message = 'How did you get this page?'
     return HttpResponse(message)
